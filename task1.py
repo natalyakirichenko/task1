85a43888006d1f51ad08fb4fda5ba8477a76d354
natalyakirichenko <natalyakirichenkouniversity@gmail.com>
2020-12-22 06:20:27 +0300

    Create task1.py

---
+++ task1.py
@@ -0,0 +1,10 @@
+a = input ("Enter the first number: ")
+b = input ("Enter the second number: ")
+c = input ("Enter the third number ")
+
+if int (a)+ int (b) + int (c)==180:
+    print ("A triangle may have the aforementioned agngles")
+
+
+else:
+    print ("A triangle with such angles doesn't exist")
